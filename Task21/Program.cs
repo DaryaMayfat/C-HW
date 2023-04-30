25f11d386c8071b486b3af1ec689c95d4ce2b093
Darya Maifat <darya885@gmail.com>
2023-04-30 20:01:47 +0600

    Расстояние между точками

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a1 = GetUserNumber("Введите значение x1: ");
+int b1 = GetUserNumber("Введите значение y1: ");
+int c1 = GetUserNumber("Введите значение z1: ");
+int a2 = GetUserNumber("Введите значение x2: ");
+int b2 = GetUserNumber("Введите значение y2: ");
+int c2 = GetUserNumber("Введите значение z2: ");
+
+double result = Math.Sqrt (Math.Pow (a2 - a1, 2) + Math.Pow (b2 - b1, 2) + Math.Pow (c2 - c1, 2));
+
+Console.WriteLine ($"A({a1}, {b1}, {c1}); B({a2}, {b2}, {c2}) -> {result:f2} ");
+
+static int GetUserNumber (string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        int userInput;
+        if (int.TryParse(Console.ReadLine(), out userInput))
+        {
+            return userInput;
+        }
+            else
+            Console.WriteLine ("Вы ввели некорректное значение!");
+        
+        }
+    }
