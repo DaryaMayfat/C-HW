20a82f56502d6a99813dc933ec0ecd1a2ab8f94e
Darya Maifat <darya885@gmail.com>
2023-04-30 22:15:01 +0600

    Таблица кубов

---
+++ Task19/Program.cs
@@ -0,0 +1,27 @@
+﻿int a=0;
+GetUserNumber(ref a);
+Palindrom (a);
+
+static void GetUserNumber (ref int a); 
+{
+    try
+    {
+        Console.WriteLine ( "Введите пятизначное число: ");
+        a = int.Parse (Console.ReadLine()??"");
+    }
+    catch(Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return;
+    }
+}
+static void Palindrom (int a);
+
+if (a%10 == a/10000 && a%10000-a%1000 == a%100-a%10)
+{
+Console.WriteLine ($"{a} -> да");
+}
+else 
+{
+    Console.WriteLine ($"{a} -> нет");
+}
